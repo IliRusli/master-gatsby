363f0d787c3f769ff40fc9c319b6b05a455349eb
ilifilza <iffyll.ann@gmail.com>
2021-10-17 14:41:08 +0000

    Implement page layout and basic routing

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 export default function HomePage() {
   return (
-    <div>
-      <p>Hey! I'm the home ppppage</p>
-    </div>
+    <>
+      <p>Hey! I'm the home page</p>
+    </>
   );
 }
--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 export default function PizzasPage() {
   return (
-    <div>
+    <>
       <p>Hey! I'm the pizzas page</p>
-    </div>
+    </>
   );
 }
