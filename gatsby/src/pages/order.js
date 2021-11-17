da74873f9f03025d48a215b2f6aa60bc97b9b9f8
ilifilza <iffyll.ann@gmail.com>
2021-11-17 13:32:48 +0000

    Add styles in order page

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -4,6 +4,8 @@ import useForm from "../utils/useForm";
 import Img from "gatsby-image";
 import calculatePizzaPrice from "../utils/calculatePizzaPrice";
 import formatMoney from "../utils/formatMoney";
+import OrderStyles from "../styles/OrderStyles";
+import MenuItemStyles from "../styles/MenuItemStyles";
 
 export default function OrderPage({ data }) {
   const { values, updateValue } = useForm({
@@ -16,7 +18,7 @@ export default function OrderPage({ data }) {
   return (
     <>
       <SEO title={"Order a pizza"} />
-      <form>
+      <OrderStyles>
         <fieldset>
           <legend>Your Info</legend>
           <label htmlFor="name">Name</label>
@@ -34,10 +36,10 @@ export default function OrderPage({ data }) {
             onChange={updateValue}
           ></input>
         </fieldset>
-        <fieldset>
+        <fieldset className="menu">
           <legend>Menu</legend>
           {pizzas.map((pizza) => (
-            <div key={pizza.id}>
+            <MenuItemStyles key={pizza.id}>
               <Img
                 width="50"
                 height="50"
@@ -52,13 +54,13 @@ export default function OrderPage({ data }) {
                   </button>
                 ))}
               </div>
-            </div>
+            </MenuItemStyles>
           ))}
         </fieldset>
-        <fieldset>
+        <fieldset className="order">
           <legend>Order</legend>
         </fieldset>
-      </form>
+      </OrderStyles>
     </>
   );
 }
