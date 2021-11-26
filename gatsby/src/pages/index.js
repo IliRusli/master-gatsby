35e48b227d3b31c0d160a460ee93365bd90f6612
ilifilza <iffyll.ann@gmail.com>
2021-11-26 16:21:59 +0000

    Fetch data from graphql endpoints

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,32 @@
 import React from "react";
+import useLatestData from "../utils/useLatestData";
+
+function CurrrentlySlicing() {
+  return (
+    <div>
+      <p>CurrrentlySlicing</p>
+    </div>
+  );
+}
+
+function HotSlices() {
+  return (
+    <div>
+      <p>HotSlices</p>
+    </div>
+  );
+}
 
 export default function HomePage() {
+  const { hotSlices, slicemasters } = useLatestData();
   return (
-    <>
-      <p>Hey! I'm the home page</p>
-    </>
+    <div className="center">
+      <h1>The Best Pizza Downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <div>
+        <CurrrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </div>
+    </div>
   );
 }
